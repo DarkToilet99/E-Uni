07f6a0f8b9f1c871a31a925d1dd6a9646d6b7051
DESKTOP-6U41IO6\DarkToilet <ane.mac99@gmail.com>
2020-11-21 21:48:46 +0200

    no message

--- connect.php
+++ connect.php
@@ -18,8 +18,22 @@
 		$nr=$row[0];
 		if($nr==0)
 		{
-				$query1=mysqli_query($conn,"insert into users(firstName,lastName,email,password) values('$firstName','$lastName','$email','$password')")
-				or die("Inserarea nu a putut avea loc!".mysqli_error($conn));
+			if($query1=mysqli_query($conn,"insert into users(firstName,lastName,email,password) values('$firstName','$lastName','$email','$password')")
+				or die("Inserarea nu a putut avea loc!".mysqli_error($conn)))
+				{
+					$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    			$charactersLength = strlen($characters);
+    			$randomString = '';
+    			for ($i = 0; $i < 6; $i++)
+					{
+        		$randomString .= $characters[rand(0, $charactersLength - 1)];
+  				}
+
+					$subject = "E-UNI Account Verification";
+					$message = "Hello! and welcome to E-UNI Here is your cod for verification: $randomString";
+					mail ( $email , $subject , $message);
+
+				}
 
 		}
 		else
