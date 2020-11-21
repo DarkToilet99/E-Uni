7ffc35827de9a9ac6d70f76bd836f0e377f47b9b
DESKTOP-6U41IO6\DarkToilet <ane.mac99@gmail.com>
2020-11-21 14:47:10 +0200

    Working register with a data base

--- connect.php
+++ connect.php
@@ -1,22 +1,35 @@
 <?php
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
+function insert_into_tabel()
+{
 
+	$firstName = $_POST["firstName"];
+	$lastName = $_POST["lastName"];
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	$id = 0;
 	//Database connection...
-	&conn = new mysqli('localhost','root','','database_name');
+	$conn = mysqli_connect('localhost','root','','users');
 	if($conn->connect_error)
 	{
 		die('Conection Failed : '.$conn->connect_error);
 	}
 	else
 	{
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, email, password) values(?,?,?,?)");
-		$stmt->bind_param("ssss", $firstName, $lastName, $email, $password);
-		$stmt->execute();
-		echo "Now you are registred";
-		$stmt->close();
-		$conn->close();
+		$query=mysqli_query($conn,"select count(*) from users where email='$email'");
+		$row=mysqli_fetch_row($query);
+		$nr=$row[0];
+		if($nr==0)
+		{
+				$query1=mysqli_query($conn,"insert into users(firstName,lastName,email,password) values('$firstName','$lastName','$email','$password')")
+				or die("Inserarea nu a putut avea loc!".mysqli_error($conn));
+		}
+		else
+		{
+				return 0;
+
+		}
+
+
 	}
-?>
+
+}
